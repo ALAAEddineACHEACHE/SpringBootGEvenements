0ded1e9edbaad663bd4963e67c6d5574a8c3ef3d
Alae <alaedineacheache@gmail.com>
2025-12-07 02:45:45 +0100

    SomeChanges

--- src/main/java/com/Gestion/Evenements/controller/EventController.java
+++ src/main/java/com/Gestion/Evenements/controller/EventController.java
@@ -18,6 +18,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/events")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class EventController {
 
     private final EventService eventService;
